c873e3463321cb57e189e16b9915853dbef8d056
ruprosperi2 <rubenjariasp@gmail.com>
2022-03-27 22:22:23 -0400

    feat: publcinado sitio

--- src/tests/components/CategoryItem.test.ts
+++
@@ -1,7 +0,0 @@
-import {describe, test} from "vitest";
-
-describe("Descripción", () => {
-    test("otra descripcion", () => {
-
-    })
-})
---
+++ src/tests/components/CategoryItem.test.tsx
@@ -0,0 +1,20 @@
+import {describe, expect, test} from "vitest";
+import {shallow} from "enzyme";
+import CategoryItem from "../../components/CategoryItem";
+import {Image} from "../../components/types";
+
+describe("Descripción", () => {
+    const title = "custom-title"
+    const url = "custom-url"
+    const image: Image = {
+        id: "custom-id",
+        title: title,
+        url: url,
+    }
+
+    test("otra descripcion", () => {
+        const wrapper = shallow(<CategoryItem image={image} />)
+
+        expect(wrapper).toMatchSnapshot()
+    })
+})
